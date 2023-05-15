392d5819fd37fd17d203b934a23c20486cfc7d45
RipNasty <det625@gmail.com>
2023-05-15 16:37:12 -0400

    added additional traits to users table

--- server/src/routes/api/v1/usersRouter.js
+++ server/src/routes/api/v1/usersRouter.js
@@ -9,10 +9,10 @@ const usersRouter = new express.Router()
 
 usersRouter.post("/", async (req, res) => {
   const formInput = cleanUserInput(req.body)
-  const { email, password, passwordConfirmation } = formInput
+  const { email, password, passwordConfirmation, firstName, age, pronouns, cityNeighborhood, experienceLevel } = formInput
 
   try {
-    const persistedUser = await User.query().insertAndFetch({ email, password })
+    const persistedUser = await User.query().insertAndFetch({ email, password, firstName, age, pronouns, cityNeighborhood, experienceLevel })
     return req.login(persistedUser, () => {
       return res.status(201).json({ user: persistedUser })
     })
