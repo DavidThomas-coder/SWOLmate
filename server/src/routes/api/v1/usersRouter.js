3bb58b5a26421b9f220c89834c4a95e9ac4ae687
RipNasty <det625@gmail.com>
2023-05-15 16:46:20 -0400

    fixed a goof

--- server/src/routes/api/v1/usersRouter.js
+++ server/src/routes/api/v1/usersRouter.js
@@ -3,19 +3,12 @@ import { ValidationError } from "objection";
 
 import { User } from "../../../models/index.js";
 
-
-usersRouter.post("/", async (req, res) => {
-  const formInput = cleanUserInput(req.body)
-  const { email, password, passwordConfirmation, firstName, age, pronouns, cityNeighborhood, experienceLevel } = formInput
-
 const usersRouter = new express.Router();
 
-
 usersRouter.post("/", async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, firstName, age, pronouns, cityNeighborhood, experienceLevel } = req.body;
   try {
-
-    const persistedUser = await User.query().insertAndFetch({ email, password, firstName, age, pronouns, cityNeighborhood, experienceLevel })
+    const persistedUser = await User.query().insertAndFetch({ email, password, firstName, age, pronouns, cityNeighborhood, experienceLevel });
     return req.login(persistedUser, () => {
       return res.status(201).json({ user: persistedUser });
     });
@@ -23,7 +16,7 @@ usersRouter.post("/", async (req, res) => {
     if (error instanceof ValidationError) {
       return res.status(422).json({ errors: error.data });
     }
-    return res.status(422).json({ errors: error });
+    return res.status(500).json({ error: errors.message });
   }
 });
 
