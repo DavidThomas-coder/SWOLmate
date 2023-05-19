5e1c5eb96586898faf9d2d49c6ccdf9d8e76e783
RipNasty <det625@gmail.com>
2023-05-19 12:01:47 -0400

    in the process of adding userPic

--- server/src/routes/api/v1/usersRouter.js
+++ server/src/routes/api/v1/usersRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ValidationError } from "objection";
 import UserSerializer from "../../serializers/UserSerializer.js";
+import uploadImage from "../../../services/uploadImage.js";
 
 import { User } from "../../../models/index.js";
 
@@ -28,6 +29,31 @@ usersRouter.post("/", async (req, res) => {
       return res.status(422).json({ errors: error.data });
     }
     return res.status(500).json({ error: errors.message });
+  }
+});
+
+usersRouter.get("/image", async (req, res) => {
+  try {
+    const userToReturn = await User.query().findById(req.user.id);
+    return res.status(200).json({ photo: userToReturn.imageUrl });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ errors: error });
+  }
+});
+
+usersRouter.post("/image", uploadImage.single("image"), async (req, res) => {
+  try {
+    const { body } = req;
+    const data = {
+      ...body,
+      image: req.file.location,
+    };
+    const user = await User.query().findById(req.user.id);
+    await user.$query().patch({ imageUrl: req.file.location });
+    return res.status(201).json({ photo: user.imageUrl });
+  } catch (error) {
+    return res.status(500).json({ errors: error });
   }
 });
 
