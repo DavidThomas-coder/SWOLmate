047ff9769744d62aa05fe80d7c5ce072e6681e79
RipNasty <det625@gmail.com>
2023-05-24 17:02:24 -0400

    migrations made, need to make models and relations

--- server/src/db/migrations/20230524183912_createGroups.cjs
+++ server/src/db/migrations/20230524183912_createGroups.cjs
@@ -8,6 +8,7 @@
 exports.up = async (knex) => {
     return knex.schema.createTable("groups", (table) => {
         table.bigIncrements("id")
+        table.integer("ownerId")
         table.string("groupName").notNullable()
         table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
         table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
