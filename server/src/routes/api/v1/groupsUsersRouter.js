c61b029f689d7c3e5bcb4d63f0ad4ba107e8ec1f
RipNasty <det625@gmail.com>
2023-06-03 12:10:34 -0400

    in progress

--- server/src/routes/api/v1/groupsUsersRouter.js
+++ server/src/routes/api/v1/groupsUsersRouter.js
@@ -1,23 +1,21 @@
-import express from "express";
-import GroupSerializer from "../../serializers/GroupSerializer.js";
-import { Group, User, Membership } from "../../../models/index.js";
+import express from "express"
+import GroupSerializer from "../../serializers/GroupSerializer.js"
+import { Group, User, Membership } from "../../../models/index.js"
 
-const groupsUsersRouter = new express.Router();
+const groupsUsersRouter = new express.Router()
 
 groupsUsersRouter.post("/", async (req, res) => {
-    const { groupId, userId } = req.body;
+    const { groupId, userId } = req.body
 
     try {
-        const group = await Group.query().findById(groupId);
-
+        const group = await Group.query().findById(groupId)
         if (!group) {
-        return res.status(404).json({ error: "Group not found" });
+        return res.status(404).json({ error: "Group not found" })
         }
 
-        const user = await User.query().findById(userId);
-
+        const user = await User.query().findById(userId)
         if (!user) {
-        return res.status(404).json({ error: "User not found" });
+        return res.status(404).json({ error: "User not found" })
         }
 
         // Check if the user is already a member of the group
@@ -26,32 +24,25 @@ groupsUsersRouter.post("/", async (req, res) => {
             groupId: group.id,
             userId: user.id,
         })
-        .first();
+        .first()
 
         if (existingMembership) {
-        return res.status(400).json({ error: "User is already a member of the group" });
+        return res.status(400).json({ error: "User is already a member of the group" })
         }
 
         // Create a new membership record to associate the user with the group
         const newMembership = await Membership.query().insert({
         groupId: group.id,
         userId: user.id,
-        });
-
-        // Optionally, you can fetch the updated group with the associated users
-        const updatedGroup = await Group.query()
-        .findById(group.id)
-        .withGraphFetched("users");
+        })
 
         // Serialize the updated group with the associated users
-        const serializedGroup = GroupSerializer.showGroupDetails(updatedGroup);
-
-        return res.status(201).json({ group: serializedGroup });
+        const serializedGroup = GroupSerializer.showGroupDetails(updatedGroup)
+        return res.status(201).json({ group: serializedGroup })
     } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: "Internal server error" });
+        console.log(error)
+        return res.status(500).json({ error: "Internal server error" })
     }
-});
+})
 
-export default groupsUsersRouter;
-
+export default groupsUsersRouter
