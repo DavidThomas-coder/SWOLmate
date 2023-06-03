e8ca0abd1482382fe2e193cd485c9697ba38151d
RipNasty <det625@gmail.com>
2023-06-03 12:00:26 -0400

    made groupsUsersRouter, still working on it

---
+++ server/src/routes/api/v1/groupsUsersRouter.js
@@ -0,0 +1,57 @@
+import express from "express";
+import GroupSerializer from "../../serializers/GroupSerializer.js";
+import { Group, User, Membership } from "../../../models/index.js";
+
+const groupsUsersRouter = new express.Router();
+
+groupsUsersRouter.post("/", async (req, res) => {
+    const { groupId, userId } = req.body;
+
+    try {
+        const group = await Group.query().findById(groupId);
+
+        if (!group) {
+        return res.status(404).json({ error: "Group not found" });
+        }
+
+        const user = await User.query().findById(userId);
+
+        if (!user) {
+        return res.status(404).json({ error: "User not found" });
+        }
+
+        // Check if the user is already a member of the group
+        const existingMembership = await Membership.query()
+        .where({
+            groupId: group.id,
+            userId: user.id,
+        })
+        .first();
+
+        if (existingMembership) {
+        return res.status(400).json({ error: "User is already a member of the group" });
+        }
+
+        // Create a new membership record to associate the user with the group
+        const newMembership = await Membership.query().insert({
+        groupId: group.id,
+        userId: user.id,
+        });
+
+        // Optionally, you can fetch the updated group with the associated users
+        const updatedGroup = await Group.query()
+        .findById(group.id)
+        .withGraphFetched("users");
+
+        // Serialize the updated group with the associated users
+        const serializedGroup = GroupSerializer.showGroupDetails(updatedGroup);
+
+        return res.status(201).json({ group: serializedGroup });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+export default groupsUsersRouter;
+
--- server/src/routes/rootRouter.js
+++ server/src/routes/rootRouter.js
@@ -4,6 +4,7 @@ import usersRouter from "./api/v1/usersRouter.js";
 import clientRouter from "./clientRouter.js";
 import chatsRouter from "./api/v1/chatsRouter.js";
 import groupsRouter from "./api/v1/groupsRouter.js";
+import groupsUsersRouter from "./api/v1/groupsUsersRouter.js";
 
 const rootRouter = new express.Router();
 
@@ -13,7 +14,7 @@ rootRouter.use("/api/v1/user-sessions", userSessionsRouter);
 rootRouter.use("/api/v1/users", usersRouter);
 rootRouter.use("/api/v1/chats", chatsRouter)
 rootRouter.use("/api/v1/groups", groupsRouter)
+rootRouter.use("/api/v1/groups/users", groupsUsersRouter)
 
-//place your server-side routes here
 
 export default rootRouter;
