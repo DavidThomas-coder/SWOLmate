e3a99a8533c153b62208663dc213bc079794ede9
RipNasty <det625@gmail.com>
2023-06-03 11:35:09 -0400

    need to add endpoint to add users to group

--- client/src/components/GroupShow.js
+++ client/src/components/GroupShow.js
@@ -11,32 +11,32 @@ const GroupShow = (props) => {
 
     const getUserChats = async () => {
         try {
-        const response = await fetch(`/api/v1/chats`);
+        const response = await fetch(`/api/v1/chats`)
         if (!response.ok) {
-            const errorMessage = `${response.status} (${response.statusText})`;
-            const error = new Error(errorMessage);
-            throw error;
+            const errorMessage = `${response.status} (${response.statusText})`
+            const error = new Error(errorMessage)
+            throw error
         }
-        const chatBody = await response.json();
-        setUserChats(chatBody.chats);
+        const chatBody = await response.json()
+        setUserChats(chatBody.chats)
         } catch (error) {
-        console.error(`Error in fetch: ${error.message}`);
+        console.error(`Error in fetch: ${error.message}`)
         }
-    };
+    }
 
     const getGroup = async () => {
         const groupId = props.match.params.id
         try {
-            const response = await fetch(`/api/v1/groups/${groupId}`)
-            if (!response.ok) {
-                const errorMessage = `${response.status} (${response.statusText})`
-                const error = new Error(errorMessage)
-                throw error
-            }
-            const body = await response.json()
-            setGroupShow(body.group)
+        const response = await fetch(`/api/v1/groups/${groupId}`)
+        if (!response.ok) {
+            const errorMessage = `${response.status} (${response.statusText})`
+            const error = new Error(errorMessage)
+            throw error
+        }
+        const body = await response.json()
+        setGroupShow(body.group)
         } catch (error) {
-            console.error(`Error in fetch: ${error.message}`)
+        console.error(`Error in fetch: ${error.message}`)
         }
     }
 
@@ -49,39 +49,44 @@ const GroupShow = (props) => {
         event.preventDefault()
 
         try {
-            const groupId = groupShow.id
-            const response = await fetch(`/api/v1/groups/${groupId}/users`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ userId }),
-            })
-            if (!response.ok) {
-                const errorMessage = `${response.status} (${response.statusText})`
-                const error = new Error(errorMessage)
-                throw error 
-            }
-            getGroup()
+        const groupId = groupShow.id
+        const response = await fetch(`/api/v1/groups/${groupId}/users`, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userId }),
+        })
+        if (!response.ok) {
+            const errorMessage = `${response.status} (${response.statusText})`
+            const error = new Error(errorMessage)
+            throw error
+        }
+        getGroup()
         } catch (error) {
-            console.error(`Error in fetch: ${error.message}`)
+        console.error(`Error in fetch: ${error.message}`)
         }
     }
 
     const chatsArray = userChats.map((chat) => {
         return (
-        <div key={chat.id}className="chat-item">
+        <div key={chat.id} className="chat-item">
             <Link to={`/chats/${chat.id}`}>{chat.title}</Link>
+            <button
+            onClick={(event) => handleAddUser(event, chat.userId)}
+            className="invite-button"
+            >
+            Invite
+            </button>
         </div>
         )
     })
-        
 
     return (
         <div className="show-page">
-            <h2 className="show-title">{groupShow.groupName}</h2>
-            <h3>Users You've Messaged With:</h3>
-                {chatsArray}
+        <h2 className="show-title">{groupShow.groupName}</h2>
+        <h3>Add A User You've Connected With:</h3>
+        {chatsArray}
         </div>
     )
 }
