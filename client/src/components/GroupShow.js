931207dcbfce7711c3e514b0167f49cbf5df5172
RipNasty <det625@gmail.com>
2023-06-11 16:54:02 -0700

    ALL users are shown in each group rather than just users in the group.  working on it

--- client/src/components/GroupShow.js
+++ client/src/components/GroupShow.js
@@ -1,104 +1,107 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const GroupShow = (props) => {
     const [groupShow, setGroupShow] = useState({
         id: "",
         groupName: "",
         notes: [],
-    })
-    const [userChats, setUserChats] = useState([])
+        users: [],
+    });
+    const [userChats, setUserChats] = useState([]);
     const [groupUsers, setGroupUsers] = useState([]);
 
     const getUserChats = async () => {
         try {
-        const response = await fetch(`/api/v1/chats`)
+        const response = await fetch(`/api/v1/chats`);
         if (!response.ok) {
-            const errorMessage = `${response.status} (${response.statusText})`
-            const error = new Error(errorMessage)
-            throw error
+            const errorMessage = `${response.status} (${response.statusText})`;
+            const error = new Error(errorMessage);
+            throw error;
         }
-        const chatBody = await response.json()
-        setUserChats(chatBody.chats)
+        const chatBody = await response.json();
+        setUserChats(chatBody.chats);
         } catch (error) {
-        console.error(`Error in fetch: ${error.message}`)
+        console.error(`Error in fetch: ${error.message}`);
         }
-    }
+    };
 
     const getGroupUsers = async (groupId) => {
         try {
-            const response = await fetch(`/api/v1/groups/${groupId}/users`)
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status} ${response.statusText}`)
-            }
-            const data = await response.json()
-            const { users } = data
-            setGroupUsers(users)
-            } catch (error) {
-            console.error("Error fetching group users:", error.message)
-            }
-        }
-
-    const getGroup = async () => {
-        const groupId = props.match.params.id
-        try {
-        const response = await fetch(`/api/v1/groups/${groupId}`)
-        if (!response.ok) {
-            const errorMessage = `${response.status} (${response.statusText})`
-            const error = new Error(errorMessage)
-            throw error
-        }
-        const body = await response.json()
-        setGroupShow(body.group)
-        getGroupUsers(groupId)
-        } catch (error) {
-        console.error(`Error in fetch: ${error.message}`)
-        }
-    }
-
-
-    useEffect(() => {
-        getUserChats()
-        getGroup()
-    }, [])
-
-    console.log(groupShow)
-    console.log(groupShow.users)
-
-    const handleAddUser = async (event, userId) => {
-        event.preventDefault();
-        
-            try {
-            const groupId = groupShow.id;
-            console.log("groupShow", groupShow)
-            console.log("groupId:", groupId);
-            console.log("userId:", userId);
-        
-            const response = await fetch(`/api/v1/groups/${groupId}/users`, {
-                method: "POST",
-                headers: {
-                "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ groupId, userId }),
-            });
-        
+            const response = await fetch(`/api/v1/groups/${groupId}/users`);
             if (!response.ok) {
                 throw new Error(`Error: ${response.status} ${response.statusText}`);
             }
-        
-            // Assuming the response contains the updated group information
             const data = await response.json();
-            const updatedGroup = data.group;
-        
-            // Update state
-            setGroupShow(updatedGroup);
-        
-            // Show a message or error
-            console.log("User added to group successfully!");
+            const { users } = data; // Extract the users array
+            setGroupUsers(users); // Set the users array in the state
             } catch (error) {
-            console.error("Error adding user:", error.message);
-            }
+            console.error("Error fetching group users:", error.message);
+        }
     };      
+
+    const getGroup = async () => {
+        const groupId = props.match.params.id;
+        try {
+        const response = await fetch(`/api/v1/groups/${groupId}`);
+        if (!response.ok) {
+            const errorMessage = `${response.status} (${response.statusText})`;
+            throw new Error(errorMessage);
+        }
+        const data = await response.json();
+        console.log("data:", data);
+        const { group } = data;
+
+        // Set the group in the state
+        setGroupShow(group);
+
+        // Fetch the group users
+        getGroupUsers(groupId);
+        } catch (error) {
+        console.error(`Error in fetch: ${error.message}`);
+        }
+    };
+
+    useEffect(() => {
+        getUserChats();
+        getGroup();
+    }, []);
+
+    console.log("groupShow:", groupShow);
+    console.log("groupUsers:", groupUsers);
+
+    const handleAddUser = async (event, userId) => {
+        event.preventDefault();
+
+        try {
+        const groupId = groupShow.id;
+
+        const response = await fetch(`/api/v1/groups/${groupId}/users`, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ groupId, userId }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+
+        // Assuming the response contains the updated group information
+        const data = await response.json();
+        console.log("data:", data);
+        const updatedGroup = data.group;
+
+        // Update state
+        setGroupShow(updatedGroup);
+
+        // Show a message or error
+        console.log("User added to group successfully!");
+        } catch (error) {
+        console.error("Error adding user:", error.message);
+        }
+    };
 
     const chatsArray = userChats.map((chat) => {
         return (
@@ -111,24 +114,26 @@ const GroupShow = (props) => {
             Invite
             </button>
         </div>
-        )
-    })
+        );
+    });
 
     return (
         <div className="show-page">
-            <h2 className="show-title">{groupShow.groupName}</h2>
-            <h3>Add A User You've Connected With:</h3>
-            {chatsArray}
+        <h2 className="show-title">{groupShow?.groupName || "Loading..."}</h2>
+        <h3>Add A User You've Connected With:</h3>
+        {chatsArray}
+        {groupUsers && (
             <div className="group-users">
-                <h2>Group Users:</h2>
-                <ul>
-                    {groupUsers.map((user) => (
-                    <li key={user.id}>{user.name}</li>
-                    ))}
-                </ul>
+            <h2>Group Users:</h2>
+            <ul>
+                {groupUsers.map((user) => (
+                <li key={user.id}>{user.firstName}</li>
+                ))}
+            </ul>
             </div>
+        )}
         </div>
-    )
-}
+    );
+};
 
-export default GroupShow
+export default GroupShow;
--- server/src/routes/api/v1/groupsUsersRouter.js
+++ server/src/routes/api/v1/groupsUsersRouter.js
@@ -17,7 +17,29 @@ groupsUsersRouter.get("/", async (req, res) => {
     }
     });
 
-    groupsUsersRouter.post("/", async (req, res) => {
+    groupsUsersRouter.get("/:id/users", async (req, res) => {
+        const { groupId } = req.params;
+    
+        try {
+            const group = await Group.query().findById(groupId);
+    
+            if (!group) {
+                return res.status(404).json({ error: "Group not found" });
+            }
+    
+            const users = await group.$relatedQuery("users");
+    
+            const serializedUsers = users.map((user) =>
+                UserSerializer.showUserDetails(user)
+            );
+    
+            return res.status(200).json({ group: group, users: serializedUsers });
+        } catch (error) {
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    });     
+
+groupsUsersRouter.post("/", async (req, res) => {
     const { groupId, userId } = req.body;
     console.log("BELOW IS REQ BODY");
     console.log(req.body);
