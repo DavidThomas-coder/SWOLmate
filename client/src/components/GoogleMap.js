87ce0d6f0e699bca7a3c3651e7afe5e575860eca
RipNasty <det625@gmail.com>
2023-09-06 14:00:23 -0400

    updates

--- client/src/components/GoogleMap.js
+++ client/src/components/GoogleMap.js
@@ -103,7 +103,7 @@ const GoogleMap = (props) => {
                     </div>
                 </div>
                 <h1>Get Active In Your Area</h1>
-                <p>We're showing you gyms by default, but choose what you're looking for below!</p>
+                <p>We're showing you gyms by default, but you can meet up anywhere!</p>
                 <div className="map-search-container">
                     <MapSearch setSearchQuery={setSearchQuery} />
                 </div>
